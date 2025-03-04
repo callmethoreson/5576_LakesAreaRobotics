c514bb907032dbdb23b344b2d6f382ff7a8ac3e9
CallMeThoreson <18athoreso@gmail.com>
2025-03-03 19:41:42 -0600

    Update ElevatorSubsystem.java

--- Brann2025/FRC Projects/Command Based Bot/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ Brann2025/FRC Projects/Command Based Bot/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -111,7 +111,7 @@ public class ElevatorSubsystem extends SubsystemBase {
                 leftSpeed = speed;
                 rightSpeed = speed;    
             }else{
-                leftSpeed = 0;asdfasdfasdfasdfsda
+                leftSpeed = 0;
                 rightSpeed = 0;    
             }
         }else{
